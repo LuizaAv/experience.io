8169014f658ae46729bfc10fb9bff0b6a324d11e
Sargis <s.torosyan83@gmail.com>
2021-05-11 10:22:55 +0400

    Added eventhandlers for loginpage

--- src/components/login.js/loginpage.js
+++ src/components/login.js/loginpage.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import "./loginpage.css"
 
 function Loginpage(){
-    
+    const [userEmail, setUserEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    let newUser = {
+        "email": userEmail, 
+        "password": password
+    }
+
+    const emailChangeHandler = (e) => {
+        setUserEmail(e.target.value)
+        //console.log(userEmail)
+    }
+
+    const passwordChangeHandler = (e) => {
+        setPassword(e.target.value)
+        console.log(password)
+    }
+
+    const clickHandler = (e) => {
+        console.log(newUser)
+    }
 
     return(
         <div>
@@ -12,16 +32,36 @@ function Loginpage(){
             <div className="introPage">
                 Log in to list your bootcamp or rate, review and favorite bootcamps
             </div>
-            <label className="emailLabel">Email Address</label>
-                <input placeholder="Enter Email" className ="emailInput"/>
-            <label className="passwordLabel">Password</label>
-                <input placeholder="Enter Password" className="passwordInput"/>
-            <button>
+            <label className="emailLabel">
+                Email Address
+            </label>
+                <input 
+                placeholder="Enter Email" 
+                className ="emailInput"
+                onChange = {emailChangeHandler}
+                />
+            <label className="passwordLabel">
+                Password
+            </label>
+                <input 
+                placeholder="Enter Password" 
+                className="passwordInput"
+                onChange = {passwordChangeHandler}
+                />
+            <button onClick={clickHandler}>
                 Login
             </button>
             <div>
-                <a className="forgotPassword" href="">Forgot password</a>
-                <a className="resetPassword" href="">Reset password</a>
+                <a 
+                className="forgotPassword" 
+                href="">
+                    Forgot password
+                </a>
+                <a 
+                className="resetPassword" 
+                href="">
+                    Reset password
+                </a>
             </div>
             <div className="mainDiv">
             </div>
